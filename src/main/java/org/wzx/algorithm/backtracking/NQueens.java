2951e4cf34b3f2701dc64fe2d5a8d7e67e65283b
Wei Zhongxiang <weizhongxiang@lsrobot.vip>
2020-09-14 11:21:07 +0800

    N皇后算法完成

--- src/main/java/org/wzx/algorithm/backtracking/NQueens.java
+++ src/main/java/org/wzx/algorithm/backtracking/NQueens.java
@@ -16,22 +16,30 @@ import java.util.List;
  * <p>来源：力扣（LeetCode）
  * <p>链接：https://leetcode-cn.com/problems/n-queens
  * <p>著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author shawn
  * @create 2020/9/12
  * @since 1.0.0
  */
 public class NQueens {
 
-    private List<List<String>> boards = new ArrayList<>();
+    private static final List<List<String>> boards = new ArrayList<>();
 
     public static void main(String[] args) {
         int n = 8;
-
+        solveNQueens(n);
+        System.out.println(boards.size());
+        for (List<String> board : boards) {
+            for (String row : board) {
+                System.out.println(row);
+            }
+            System.out.println("---------------------------------------");
+        }
     }
 
     static void solveNQueens(int n) {
         List<String> board = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             char[] chars = new char[n];
             Arrays.fill(chars, '.');
             board.add(new String(chars));
@@ -43,6 +51,52 @@ public class NQueens {
 
     static void backtrack(List<String> board, int row) {
         // 触发结束条件
+        if (row == board.size()) {
+            boards.add(new ArrayList<>(board));
+            return;
+        }
 
+        for (int col = 0; col < board.size(); col++) {
+            if (!isValid(board, row, col)) {
+                continue;
+            }
+
+            // 做选择
+            char[] array = board.get(row).toCharArray();
+            array[col] = 'Q';
+            board.set(row, new String(array));
+
+            // 进入下一行决策
+            backtrack(board, row + 1);
+
+            // 撤销选择
+            array[col] = '.';
+            board.set(row, new String(array));
+        }
+    }
+
+    static boolean isValid(List<String> board, int row, int col) {
+        // 同一列上方
+        for (int i = 0; i < row; i++) {
+            if (board.get(i).charAt(col) == 'Q') {
+                return false;
+            }
+        }
+
+        // 左上方
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board.get(i).charAt(j) == 'Q') {
+                return false;
+            }
+        }
+
+        // 右上方
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++) {
+            if (board.get(i).charAt(j) == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
